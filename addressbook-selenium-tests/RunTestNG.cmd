5f144911d3f05d990e4ae24a406ebfb7f11ed75a
baranovRP <baranov.r.p@gmail.com>
2013-09-11 22:55:09 +0300

    modified cmd

--- addressbook-selenium-tests/RunTestNG.cmd
+++ addressbook-selenium-tests/RunTestNG.cmd
@@ -5,7 +5,8 @@ set runClass=org.testng.TestNG
 set libDir=D:\software-testering.ru\tools\xstream-1.4.4\lib
 set seleniumDir=D:\software-testering.ru\pft\tools
 set logFile=results.log
+set configFile=firefox.properties
 
-java -cp bin;"%libDir%\xstream-1.4.4.jar";"%libDir%\xstream\xmlpull-1.1.3.1.jar";"%libDir%\xstream\xpp3_min-1.1.4c.jar";"%seleniumDir%\selenium-server-standalone-2.33.0.jar" -DconfigFile=firefox.properties %runClass% %customSuite%  >> %logFIle%
+java -cp bin;"%libDir%\xstream-1.4.4.jar";"%libDir%\xstream\xmlpull-1.1.3.1.jar";"%libDir%\xstream\xpp3_min-1.1.4c.jar";"%seleniumDir%\selenium-server-standalone-2.33.0.jar" -DconfigFile=%configFile% %runClass% %customSuite%  >> %logFIle%
 
 pause
