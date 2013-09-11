dddf18f27798a8800c5291f25f1bbfbef74abbdf
baranovRP <baranov.r.p@gmail.com>
2013-09-11 23:54:07 +0300

    added test methods testVariousFoldersCreation, testFoldersWithSameCreation

--- addressbook-java-tests/src/com/baranov/pft/fw/FolderHelper.java
+++ addressbook-java-tests/src/com/baranov/pft/fw/FolderHelper.java
@@ -2,8 +2,11 @@ package com.baranov.pft.fw;
 
 import java.util.ArrayList;
 
+import javax.swing.JDialog;
+
 import org.netbeans.jemmy.operators.JButtonOperator;
 import org.netbeans.jemmy.operators.JDialogOperator;
+import org.netbeans.jemmy.operators.JLabelOperator;
 import org.netbeans.jemmy.operators.JTextFieldOperator;
 import org.netbeans.jemmy.operators.JTreeOperator;
 
@@ -25,10 +28,34 @@ public class FolderHelper {
 		return new Folders(list);
 	}
 
-	public void createFolder(String folderName) {
+	public String createFolder(String folderName) {
 		manager.getMenuHelper().pushCreateFolder();
 		JDialogOperator dialog = new JDialogOperator(manager.getApplication());
 		new JTextFieldOperator(dialog).setText(folderName);
 		new JButtonOperator(dialog, "OK").push();
+		return waitMessageDialog("Warning", 3000);
+
+	}
+
+	private String waitMessageDialog(String title, int timeout) {
+		long start = System.currentTimeMillis();
+		long currentTime = start;
+		while (currentTime < start + timeout) {
+			JDialog dialog = JDialogOperator.findJDialog(manager
+					.getApplication().getOwner(), title, false, false);
+			if (dialog != null) {
+				JDialogOperator dialogOp = new JDialogOperator(dialog);
+				String message = new JLabelOperator(dialogOp).getText();
+				dialogOp.requestClose();
+				return message;
+			}
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			currentTime = System.currentTimeMillis();
+		}
+		return null;
 	}
 }
--- addressbook-java-tests/src/com/baranov/pft/tests/TestFolderCreation.java
+++ addressbook-java-tests/src/com/baranov/pft/tests/TestFolderCreation.java
@@ -1,7 +1,10 @@
 package com.baranov.pft.tests;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 
 import org.testng.annotations.Test;
 
@@ -15,5 +18,31 @@ public class TestFolderCreation extends TestBase {
 		app.getFolderHelper().createFolder(folder);
 		Folders newFolders = app.getFolderHelper().getFolders();
 		assertThat(newFolders, equalTo(oldFolders.withAdded(folder)));
+	}
+
+	@Test
+	public void testVariousFoldersCreation() {
+		String folder1 = "newfolder1";
+		String folder2 = "newfolder2";
+		Folders oldFolders = app.getFolderHelper().getFolders();
+		assertThat(app.getFolderHelper().createFolder(folder1), is(nullValue()));
+		Folders newFolders = app.getFolderHelper().getFolders();
+		assertThat(newFolders, equalTo(oldFolders.withAdded(folder1)));
+		assertThat(app.getFolderHelper().createFolder(folder2), is(nullValue()));
+		Folders newFolders2 = app.getFolderHelper().getFolders();
+		assertThat(newFolders2, equalTo(newFolders.withAdded(folder2)));
+	}
+
+	@Test
+	public void testFoldersWithSameCreation() {
+		String folder = "newfolder3";
+		Folders oldFolders = app.getFolderHelper().getFolders();
+		assertThat(app.getFolderHelper().createFolder(folder), is(nullValue()));
+		Folders newFolders = app.getFolderHelper().getFolders();
+		assertThat(newFolders, equalTo(oldFolders.withAdded(folder)));
+		assertThat(app.getFolderHelper().createFolder(folder),
+				containsString("Duplicated folder name"));
+		Folders newFolders2 = app.getFolderHelper().getFolders();
+		assertThat(newFolders2, equalTo(newFolders));
 	}
 }
